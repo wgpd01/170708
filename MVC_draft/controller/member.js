50dac429688971411d1f2b6564ee78deaaf682b1
Wolfgang Chien <sakalabahonda@gmail.com>
2017-07-08 07:20:36 +0000

    EasyMVC v0.1

--- MVC_draft/controller/member.js
+++
@@ -1,9 +0,0 @@
-module.exports = function (req, res) {
-    this.req = req;
-    this.res = res;
-	this.index = function () {
-	    // this.res.send("member controller");
-		res.render('member/index.html', {
-		});
-	}
-}
